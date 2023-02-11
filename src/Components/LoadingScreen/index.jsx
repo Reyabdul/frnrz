0f1c1d0d1b8f81907bac0f017f7bbc1368d4fd23
Akash <adesigns>
2023-02-11 16:13:42 -0500

    added build

--- src/Components/LoadingScreen/index.jsx
+++ src/Components/LoadingScreen/index.jsx
@@ -1,27 +1,11 @@
 import React, { useEffect, useState } from "react";
 import $ from "jquery";
 
-import loadingVideo_DESKTOP from "../../Assets/media/static_media/loading/DESKTOP.mp4";
 import loadingVideo_MOBILE from "../../Assets/media/static_media/loading/MOBILE.mp4";
 
 import "./index.css";
 
 const LoadingScreen = () => {
-    window.onload = () => {
-        let video = document.getElementById("loading-video");
-        let source = document.createElement("source");
-
-        if (window.innerWidth <= 400) {
-            source.setAttribute("src", loadingVideo_MOBILE);
-            source.setAttribute("type", "video/mp4");
-        } else {
-            source.setAttribute("src", loadingVideo_DESKTOP);
-            source.setAttribute("type", "video/mp4");
-        }
-
-        video.appendChild(source);
-        video.play();
-    }
 
     const hideLoadingScreen = () => $(".loading-screen").fadeOut(1000);
 
@@ -29,7 +13,9 @@ const LoadingScreen = () => {
 
         <>
             <div className="loading-screen">
-                <video id="loading-video" autoPlay={true} muted loop={true} preload="none"></video> {/*onEnded={hideLoadingScreen}*/}
+                <video id="loading-video" autoPlay={true} muted loop={true} preload="none">
+                    <source src={loadingVideo_MOBILE} type="video/mp4" />    
+                </video> {/*onEnded={hideLoadingScreen}*/}
             </div>
         </>
 
