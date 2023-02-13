0e8cb81135726d1f8a826e78832e4d0a9ab7ab1f
Akash <adesigns>
2023-02-13 16:58:02 -0500

    added useCdn to client

--- src/client.js
+++ src/client.js
@@ -2,5 +2,7 @@ import sanityClient from "@sanity/client";
 
 export default sanityClient({
     projectId: 'ufskghtn',
-    dataset: 'production'
+    dataset: 'production',
+    apiVersion: "2023-02-13",
+    useCdn: true
 });
